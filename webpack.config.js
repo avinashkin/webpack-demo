14e0d0a2a8039b31b1cc23305a17f811cb3f106b
Avinash Gupta <guptaavinash92@gmail.com>
2024-03-04 18:30:29 +0000

    Adding loader to handle CSS

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,13 @@ module.exports = {
     output: {
         filename: "main.js", // the name of file in which minified code will be exported
         path: path.resolve(__dirname, "dist") // tells the directory where to write the minified code
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"] // maintain the order of array i.e. first CSS will be converted to JS by "css-loader" and then it will be injected into DOM by "style-loader"
+            }
+        ]
     }
 }
