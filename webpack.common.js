42efe1cfa10ad5c635a2ff858ee599eb0bc0f17d
Avinash Gupta <guptaavinash92@gmail.com>
2024-03-15 13:50:12 +0000

    Add dev and prod configs. Ading html-loader and clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development", // dev - tells webpack to stop minifying or prod(default)
     entry: "./src/index.js",
-    output: {
-        filename: "main.[contenthash].js", // the name of file in which minified code will be exported
-        path: path.resolve(__dirname, "dist") // tells the directory where to write the minified code
-    },
     plugins: [new HtmlWebpackPlugin({
         template: "./src/template.html"
     })],
@@ -20,6 +15,14 @@ module.exports = {
                 "css-loader", // 2. turns css to commonJS
                 "sass-loader" // 1. turns sass to css
             ] // maintain the order of array i.e. first CSS will be converted to JS by "css-loader" and then it will be injected into DOM by "style-loader"
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\/(svg|png|jpg|gif)$/,
+                use: "asset/resource"
             }
         ]
     }
