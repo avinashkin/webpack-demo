9e235cd5ac2e666a87cddad1a7503c87e2305110
Avinash Gupta <guptaavinash92@gmail.com>
2024-03-18 21:12:47 +0000

    Add entry point for vendor.js and bootstrap js

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ const {merge} = require("webpack-merge");
 module.exports = merge(common, {
     mode: "development", // dev - tells webpack to stop minifying or prod(default)
     output: {
-        filename: "main.js", // the name of file in which minified code will be exported
+        filename: "[name].bundle.js", // the name of file in which minified code will be exported
         path: path.resolve(__dirname, "dist"), // tells the directory where to write the minified code
         assetModuleFilename: "images/[name][ext]"
     }
--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 module.exports = merge(common, {
     mode: "production", // dev - tells webpack to stop minifying or prod(default)
     output: {
-        filename: "main.[contenthash].js", // the name of file in which minified code will be exported
+        filename: "[name].[contenthash].bundle.js", // the name of file in which minified code will be exported
         path: path.resolve(__dirname, "dist"), // tells the directory where to write the minified code
         assetModuleFilename: "images/[name].[hash][ext]",
     },
